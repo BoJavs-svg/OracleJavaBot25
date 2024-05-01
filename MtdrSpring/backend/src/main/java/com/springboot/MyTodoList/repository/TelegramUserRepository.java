bab1bb959be02e3bf30b38ef13822c1af8fa7eae
BoJavs <javiereric1309@gmail.com>
2024-05-01 15:57:18 -0600

    Try

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/repository/TelegramUserRepository.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/repository/TelegramUserRepository.java
@@ -8,10 +8,15 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.transaction.Transactional;
+import org.springframework.data.jpa.repository.Query;
 
 @Repository
 @Transactional
 @EnableTransactionManagement
 public interface TelegramUserRepository extends JpaRepository<TelegramUser, Long> {
     boolean existsByAccount(Long accountID);
+
+    @Query(value = "SELECT * FROM TELEGRAMUSER", nativeQuery = true)
+    void getAllUsers();
+
 }
