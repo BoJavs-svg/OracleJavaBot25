ecb0ba62219777de4612aa08d01583c1335bfe2e
BoJavs <javiereric1309@gmail.com>
2024-05-02 11:57:01 -0600

    Help

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/service/TelegramUserService.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/service/TelegramUserService.java
@@ -1,16 +1,16 @@
 package com.springboot.MyTodoList.service;
 
-import com.springboot.MyTodoList.model.TelegramUser;
-import com.springboot.MyTodoList.repository.TelegramUserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.Optional;
 
 import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springboot.MyTodoList.model.TelegramUser;
+import com.springboot.MyTodoList.repository.TelegramUserRepository;
+
 
 @Service
-@Transactional
 public class TelegramUserService {
 
     @Autowired
