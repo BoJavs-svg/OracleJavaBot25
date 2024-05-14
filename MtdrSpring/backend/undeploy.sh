d328968f7a515058cf4403a2fafb392b26633aa9
BoJavs-svg <javiereric1309@gmail.com>
2024-05-14 21:31:51 +0000

    Output logs

--- MtdrSpring/backend/undeploy.sh
+++ MtdrSpring/backend/undeploy.sh
@@ -1,3 +1,6 @@
 echo delete frontend deployment and service...
 kubectl -n mtdrworkshop delete deployment todolistapp-springboot-deployment
 kubectl -n mtdrworkshop delete service todolistapp-springboot-service
+kubectl -n mtdrworkshop delete pods --all
+kubectl -n mtdrworkshop delete services --all
+rm -rf *yaml
