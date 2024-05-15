f32e7a164a7a895b1a16387b30c3b828a6690d84
Javier Hernández <75763393+BoJavs-svg@users.noreply.github.com>
2024-05-14 18:20:11 -0600

    Update BotCommands.java

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/util/BotCommands.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/util/BotCommands.java
@@ -1,13 +1,37 @@
 package com.springboot.MyTodoList.util;
 
 public enum BotCommands {
-
-	START_COMMAND("/start"), 
-	HIDE_COMMAND("/hide"), 
 	TODO_LIST("/todolist"),
 	CHECK_TASKS("/mytasks"),
 	ADD_TASK("/addtask");
 
+	// User Commands
+    	START_COMMAND("/start"),
+    	HIDE_COMMAND("/hide"),
+    	CREATE_USER("/createuser"),
+    	EDIT_USER("/edituser"),
+    	DELETE_USER("/deleteuser"),
+
+    	// Sprint Commands
+    	CREATE_SPRINT("/createsprint"),
+    	VIEW_ALL_SPRINT_TASKS("/viewsprinttasks"),
+    	EDIT_SPRINT("/editsprint"),
+    	DELETE_SPRINT("/deletesprint"),
+
+    	// Team Commands
+    	CREATE_TEAM("/createteam"),
+    	VIEW_TEAM_TASKS("/viewteamtasks"),
+    	EDIT_TEAM("/editteam"),
+    	DELETE_TEAM("/deleteteam"),
+	
+    	// Task Commands
+    	CREATE_TASK("/createtask"),
+    	EDIT_TASK("/edittask"),
+    	FINISH_TASK("/finishtask"),
+    	VIEW_USER_TASKS("/viewusertasks"),
+	VIEW_DEVELOPER_TASKS("/mytasks"),
+	VIEW_TASK("/viewtask"),
+	DELETE_TASK("/deletetask"),
 
 	private String command;
 
